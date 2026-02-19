fa6394fd3977e92bc158663deb9a2c97cd4c8c4c
willilix <willilix@gmail.com>
2026-02-19 15:41:46 +0000

    Update tournaments_tab.py

--- modules/tournaments_tab.py
+++ modules/tournaments_tab.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 from modules.storage import read_torneios, read_sheet, save_inscricao, normalize_phone
 
+from modules.admin_gate import is_admin
 
 class _BytesUpload:
     """Fallback object that mimics Streamlit's UploadedFile enough for our storage layer."""
